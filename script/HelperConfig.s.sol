1239b323750da49a047c560d6656e14744c00b7f
garlicdoggoe <garlicspoofiyyy@gmail.com>
2025-06-07 11:18:24 +0800

    add: anvil mock price feed

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
 contract HelperConfig is Script {
     // If we are on a local anvil node, we deploy mocks
@@ -21,7 +22,7 @@ contract HelperConfig is Script {
         } else if (block.chainid == 1) {
             activeNetworkConfig = getMainnetEthConfig();
         } else {
-            // activeNetworkConfig = getAnvilEthConfig();
+            activeNetworkConfig = getAnvilEthConfig();
         }
     }
 
@@ -41,11 +42,16 @@ contract HelperConfig is Script {
         return mainnetConfig;
     }
 
-    // function getAnvilEthConfig() public pure returns (NetworkConfig memory) {
-    //     // price feed address
-    //     NetworkConfig memory anvilConfig = NetworkConfig({
-    //         priceFeed:
-    //     });
-    //     return anvilConfig;
-    // }
+    function getAnvilEthConfig() external returns (NetworkConfig memory) {
+        // price feed address
+
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(8, 2000e8); // decimal of eth/usd is 8
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed: address(mockPriceFeed)
+        });
+        return anvilConfig;
+    }
 }
