bfad521d68142aaaf6e02a8a463f1f9de295b4ae
garlicdoggoe <garlicspoofiyyy@gmail.com>
2025-06-07 11:09:34 +0800

    add: helper config and updated deployment for a moch price feed

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -4,12 +4,19 @@ pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol"; // we need to import the script contract
 import {FundMe} from "../src/FundMe.sol"; // we need to import the fund me
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 // we need to inherit the script contract
 contract DeployFundMe is Script {
     function run() external returns (FundMe) {
+        // Before startBroadcast -> Not a "real" tx
+        HelperConfig helperConfig = new HelperConfig();
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
+
+        // After startBroadcast -> A "real" tx
         vm.startBroadcast();
-        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        // Mock
+        FundMe fundMe = new FundMe(ethUsdPriceFeed);
         vm.stopBroadcast();
         return fundMe;
     }
