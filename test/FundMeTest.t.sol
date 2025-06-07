25ca4f21435bbcf188be66b43d4901b8aa1be24c
garlicdoggoe <garlicspoofiyyy@gmail.com>
2025-06-07 22:49:55 +0800

    add: local variable to avoid causing extra gas

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -107,4 +107,31 @@ contract FundMeTest is Test {
             fundMe.getOwner().balance
         );
     }
+
+    function testWithdrawFromMultipleFundersCheaper() public funded {
+        uint160 numberOfFunders = 10;
+        uint160 startingFunderIndex = 1; // 0 when wrinting a test
+        for (uint160 i = startingFunderIndex; i < numberOfFunders; i++) {
+            // vm.prank new address
+            // vm.deal new address
+            // address()
+            hoax(address(i), SEND_VALUE);
+            fundMe.fund{value: SEND_VALUE}();
+        }
+
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        // Act
+        vm.startPrank(fundMe.getOwner());
+        fundMe.cheaperWithdraw();
+        vm.stopPrank();
+
+        // Assert
+        assertEq(address(fundMe).balance, 0);
+        assertEq(
+            startingOwnerBalance + startingFundMeBalance,
+            fundMe.getOwner().balance
+        );
+    }
 }
